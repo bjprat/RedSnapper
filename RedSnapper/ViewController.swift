2e9ed3f7d9bc5871eacdf3acf9ac84ff1b149e5a
bjprat <bjprat@gmail.com>
2014-10-10 01:38:53 -0400

    Initial Commit

---
+++ RedSnapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedSnapper
+//
+//  Created by Bernard Prat on 10/10/14.
+//  Copyright (c) 2014 Pratley LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
